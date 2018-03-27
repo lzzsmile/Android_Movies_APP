1d3bcc5f896639f49c4f7e384ee88b63418859dc
lzzsmile <32747784+lzzsmile@users.noreply.github.com>
2018-03-26 23:40:23 -0600

    Update layout

---
+++ Movies/app/src/main/java/zhuangzhi/android/movies/FetchReviewsTask.java
@@ -0,0 +1,18 @@
+package zhuangzhi.android.movies;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+
+import zhuangzhi.android.movies.network.Review;
+
+/**
+ * Created by zhuangzhili on 2018-03-26.
+ */
+
+public class FetchReviewsTask extends AsyncTask<Long, Void, ArrayList<Review>> {
+
+    private final static String LOG_TAG = FetchReviewsTask.class.getCanonicalName();
+
+
+}
